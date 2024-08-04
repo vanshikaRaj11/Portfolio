c98bfa37dd6704846e2152c687ea0075a349498e
Vanshika Rajput <vanshikavce19@gmail.com>
2024-08-04 22:27:16 +0530

    Inital set up done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import logo from "../assets/about.jpg"
+const Navbar = () => {
+  return (
+      <nav className='mb-20 flex items-center justify-between bg-violet-600 py-6'>
+          <div className='flex flex-shrink-0 items-center'>
+              {/* <img src={logo} alt=''></img> */}
+          </div>
+      
+    </nav>
+  )
+}
+
+export default Navbar
