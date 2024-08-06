039d177a8db3dac4c6fc05f4c9ca0856ae2b2c23
Vanshika Rajput <vanshikavce19@gmail.com>
2024-08-06 22:43:44 +0530

    background image fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar";
 
-
 function App() {
   return (
-    <div className="App bg-black">
-    <Navbar />
+    <div class="relative  top-0 z-[-2] h-screen w-screen bg-neutral-900">
+      <div class="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]">
+        <Navbar />
+      </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from "react";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 // import logo from "../assets/about.jpg"
 const Navbar = () => {
   return (
-      <nav className='mb-20 flex items-center justify-between bg-violet-600 py-6'>
-          <div className='flex flex-shrink-0 items-center'>
-              {/* <img src={logo} alt=''></img> */}
-          </div>
-      
-    </nav>
-  )
-}
+    <nav className="mb-20 flex items-center justify-between py-6 z-10">
+      <div className="flex flex-shrink-0 items-center text-white justify-center m-6">
+      <p className="text-white">Vanshika Rajput</p>
+      </div>
 
-export default Navbar
+      <div className="flex gap-2 items-center justify-center text-2xl m-8 text-white">
+        <FaLinkedin />
+        <FaGithub />
+        <FaXTwitter />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
