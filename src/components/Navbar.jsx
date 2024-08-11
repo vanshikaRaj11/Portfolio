826876a65795cd6315dd2f66e137defb516b532b
Vanshika Rajput <vanshikavce19@gmail.com>
2024-08-11 20:50:18 +0530

    navbar changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6 z-10">
       <div className="flex flex-shrink-0 items-center text-white justify-center m-6">
-      <p className="text-white">Vanshika Rajput</p>
+      <p className="text-white">VR</p>
       </div>
 
       <div className="flex gap-2 items-center justify-center text-2xl m-8 text-white">
