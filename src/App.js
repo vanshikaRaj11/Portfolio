8f11c834792a31623474a6b743d02774c4ee6ba9
Vanshika Rajput <vanshikavce19@gmail.com>
2024-08-16 21:45:38 +0530

    Fixed navbar issue

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div class="relative  top-0 z-[-2] h-screen w-screen bg-neutral-900">
-      <div class="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]">
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="relative  top-0 z-[-2] h-screen w-screen bg-neutral-900">
+        <div className="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]"></div>
         <Navbar />
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,17 +6,19 @@ import { FaLinkedin } from "react-icons/fa";
 // import logo from "../assets/about.jpg"
 const Navbar = () => {
   return (
-    <nav className="mb-20 flex items-center justify-between py-6 z-10">
-      <div className="flex flex-shrink-0 items-center text-white justify-center m-6">
-      <p className="text-white">VR</p>
-      </div>
+    <>
+      <nav className="mb-20 flex items-center justify-between py-6 z-100">
+        <div className="flex flex-shrink-0 items-center text-white justify-center m-6">
+          <p className="text-white">VR</p>
+        </div>
 
-      <div className="flex gap-2 items-center justify-center text-2xl m-8 text-white">
-        <FaLinkedin />
-        <FaGithub />
-        <FaXTwitter />
-      </div>
-    </nav>
+        <div className="flex gap-2 items-center justify-center text-2xl m-8 text-white">
+          <FaLinkedin />
+          <FaGithub />
+          <FaXTwitter />
+        </div>
+      </nav>
+    </>
   );
 };
 
