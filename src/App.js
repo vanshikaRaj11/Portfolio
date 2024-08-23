1fb9ebbbf2f48fc7ef5c11ca3b812012927a1bce
Vanshika Rajput <vanshikavce19@gmail.com>
2024-08-23 22:43:19 +0530

    Added half hero component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -5,7 +6,11 @@ function App() {
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="relative  top-0 z-[-2] h-screen w-screen bg-neutral-900">
         <div className="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]"></div>
-        <Navbar />
+
+        <div className="container mx-auto px-8">
+          <Navbar />
+          <Hero />
+        </div>
       </div>
     </div>
   );
