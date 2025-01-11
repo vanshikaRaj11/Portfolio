39b062608d2209dc29a8c79ee0d2ab0b1c6bb229
Vanshika Rajput <vanshikavce19@gmail.com>
2025-01-11 21:06:09 +0530

    Add professional description and title to Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,18 @@ const Hero = () => {
             <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
               Vanshika Rajput
             </h1>
+
+            <span className="bg-gradient-to-r from-pink-300 via-slate-300 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+              MERN Stack Developer
+            </span>
+            <p className="my-2 max-xl py-6 font-light tracking-tighter">
+              I am a passionate backend developer with expertise in Node.js,
+              Express.js, and MongoDB. I specialize in crafting robust, scalable
+              server-side applications, designing RESTful APIs, and optimizing
+              database management. My goal is to deliver secure, reliable, and
+              high-performance solutions tailored to the needs of modern web and
+              mobile platforms.
+            </p>
           </div>
         </div>
       </div>
