30d834a1b4990a1b4e567181487dc38fdd0e2e89
Vanshika Rajput <vanshikavce19@gmail.com>
2025-01-14 15:03:25 +0530

    Initialize Vite and React project structure with essential configurations and components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { AiOutlineLinkedin } from 'react-icons/ai';
+import { FaGithubSquare, FaInstagram } from 'react-icons/fa'
+import { FaSquareXTwitter } from 'react-icons/fa6';
+
+const Footer = () => {
+  return (
+    <div className="max-w-[1300px] mx-auto flex justify-between p-6 md:p-20 text-sm md:text-lg mt-12">
+      <div className="space-y-4">
+        <h3 className="text-2xl text-gray-200 font-semibold">Vanshika Rajput</h3>
+        <div className="flex flex-row gap-6 text-gray-400 text-4xl">
+          <a href="https://github.com/vanshikaRaj11">
+            <FaGithubSquare />
+          </a>
+          <a href="https://www.linkedin.com/in/vanshika-rajput-aaa920227/">
+            <AiOutlineLinkedin />
+          </a>
+          <a href="https://x.com/Vanshika_raj11">
+            <FaSquareXTwitter />
+          </a>
+        </div>
+      </div>
+
+      <p className="text-gray-400">@2025 Vanshika_raj11</p>
+    </div>
+  );
+}
+
+export default Footer
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,201 @@
 import React from "react";
+import sidepic from "../assets/sidepic.jpeg";
+import { TypeAnimation } from "react-type-animation";
+import ShinyEffect from "./ShinyEffect";
+import { AiOutlineGithub, AiOutlineLinkedin } from "react-icons/ai";
+import { FaGitAlt, FaSquareXTwitter } from "react-icons/fa6";
+import {
+  DiJavascript1,
+  DiMongodb,
+  DiNodejsSmall,
+  DiReact,
+} from "react-icons/di";
+import { motion } from "framer-motion";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiExpress, SiJira, SiPostman } from "react-icons/si";
+import { VscVscode } from "react-icons/vsc";
 
 const Hero = () => {
   return (
-    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
-      <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2">
-          <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
-              Vanshika Rajput
-            </h1>
+    <div className="mt-24 max-w-[1200px] mx-auto relative">
+      <div className="grid md:grid-cols-2 place-items-center gap-8">
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 1 }}
+        >
+          <TypeAnimation
+            sequence={["MERN Stack Developer", 1000, "Backend Developer", 1000]}
+            speed={50}
+            repeat={Infinity}
+            className="font-bold text-gray-400 text-xl md:text-5xl italic- mb-4"
+          />
 
-            <span className="bg-gradient-to-r from-pink-300 via-slate-300 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
-              MERN Stack Developer
-            </span>
-            <p className="my-2 max-xl py-6 font-light tracking-tighter">
-              I am a passionate backend developer with expertise in Node.js,
-              Express.js, and MongoDB. I specialize in crafting robust, scalable
-              server-side applications, designing RESTful APIs, and optimizing
-              database management. My goal is to deliver secure, reliable, and
-              high-performance solutions tailored to the needs of modern web and
-              mobile platforms.
-            </p>
-          </div>
+          <motion.p
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 0.5 }}
+            className="text-gray-200 md:text-7xl text-5xl tracking-tight mb-4"
+          >
+            HEY, I AM <br />
+            <span className="text-purple-500">VANSHIKA RAJPUT</span>
+          </motion.p>
+
+          <motion.p
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 1 }}
+            className="text-gray-300 max-w-[300px] md:max-w-[500px] md:text-2xl text-lg mb-6"
+          >
+            I am a passionate backend developer with expertise in Node.js,
+            Express.js, and MongoDB. I specialize in crafting robust, scalable
+            server-side applications, designing RESTful APIs, and optimizing
+            database management.
+            {/* 
+             My goal is to deliver secure, reliable, and
+            high-performance solutions tailored to the needs of modern web and
+            mobile platforms.{" "} */}
+          </motion.p>
+
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 1.5 }}
+            className="flex flex-row items-center gap-6 my-4 md:mb-0"
+          >
+            <motion.button
+              whileHover={{
+                scale: 1.05,
+                boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.3)",
+              }}
+              className="z-10 cursor-pointer font-bold text-gray-200 md:w-auto p-4 border
+                                     border-purple-400 rounded-xl"
+            >
+              Download CV
+            </motion.button>
+
+            <div className="flex gap-6 flex-row text-4xl md:text-6xl text-purple-400 z-20">
+              <motion.a
+                whileHover={{ scale: 1.2 }}
+                href="https://github.com/vanshikaRaj11"
+              >
+                <AiOutlineGithub />
+              </motion.a>
+
+              <motion.a
+                whileHover={{ scale: 1.2 }}
+                href="https://www.linkedin.com/in/vanshika-rajput-aaa920227/"
+              >
+                <AiOutlineLinkedin />
+              </motion.a>
+
+              <motion.a
+                whileHover={{ scale: 1.2 }}
+                href="https://x.com/Vanshika_raj11"
+              >
+                <FaSquareXTwitter />
+              </motion.a>
+            </div>
+          </motion.div>
+        </motion.div>
+
+        <motion.img
+          src={sidepic}
+          className="w-[300px] md:w-[450px]"
+          initial={{ opacity: 0, scale: 0.8 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 1 }}
+        />
+      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1, delay: 2 }}
+        className="flex flex-col items-center text-center px-6 md:px-0 w-full py-24"
+      >
+        <p className="text-gray-200 text-4xl md:text-5xl font-semibold mb-8">
+          My Tech Stack
+        </p>
+        <div className="grid grid-cols-3 md:grid-cols-5 gap-8">
+          {[
+            {
+              icon: <RiTailwindCssFill className="text-blue-600 text-4xl" />,
+              name: "Tailwind CSS",
+              id: 1,
+            },
+            {
+              icon: <DiJavascript1 className="text-yellow-500 text-4xl" />,
+              name: "JavaScript",
+              id: 2,
+            },
+            {
+              icon: <DiReact className="text-blue-500 text-4xl" />,
+              name: "React",
+              id: 3,
+            },
+            {
+              icon: <DiNodejsSmall className="text-green-500 text-4xl" />,
+              name: "Node.js",
+              id: 4,
+            },
+            {
+              icon: <DiMongodb className="text-green-600 text-4xl" />,
+              name: "MongoDB",
+              id: 5,
+            },
+            {
+              icon: <SiExpress className="text-gray-400 text-4xl" />,
+              name: "Express.js",
+              id: 6,
+            },
+            {
+              icon: <SiPostman className="text-orange-700 text-4xl" />,
+              name: "Postman",
+              id: 7,
+            },
+            {
+              icon: <SiJira className="text-blue-500 text-4xl" />,
+              name: "Jira",
+              id: 8,
+            },
+            {
+              icon: <FaGitAlt className="text-orange-700 text-4xl" />,
+              name: "Git",
+              id: 9,
+            },
+            {
+              icon: <VscVscode className="text-blue-500 text-4xl" />,
+              name: "VS Code",
+              id: 10,
+            },
+          ].map((item) => (
+            <motion.div
+              key={item.id}
+              whileHover={{ scale: 1.2 }}
+              className="group relative flex items-center justify-center w-20 h-20 rounded-md shadow-md bg-gray-800 transition-all duration-300 hover:border-purple-500 hover:border-2"
+            >
+              {/* Icon */}
+              <div className="group-hover:bg-gray-700 w-full h-full flex items-center justify-center rounded-md">
+                {item.icon}
+              </div>
+
+              {/* Tooltip */}
+              <span className="absolute bottom-[-1.5rem] left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 text-sm text-gray-300 bg-gray-700 px-2 py-1 rounded-md shadow-md">
+                {item.name}
+              </span>
+            </motion.div>
+          ))}
         </div>
+      </motion.div>
+
+      <div className="absolute inset-0 hidden md:block">
+        <ShinyEffect left={0} top={0} size={1400} />
       </div>
     </div>
   );
